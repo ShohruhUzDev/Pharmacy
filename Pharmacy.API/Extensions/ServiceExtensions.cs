e5a36b6a11c4fc4f360bb74630ea77a34ceae1a5
ShohruhUzDev <shohruh7464784@mail.ru>
2023-04-27 17:21:00 +0500

    Add yml file

--- Pharmacy.API/Extensions/ServiceExtensions.cs
+++ Pharmacy.API/Extensions/ServiceExtensions.cs
@@ -2,84 +2,68 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using Pharmacy.Data.IRepositories;
-using Pharmacy.Data.Repositories;
-using Pharmacy.Service.Interfaces;
-using Pharmacy.Service.Services;
 
 namespace Pharmacy.Api.Extensions
 {
     public static class ServiceExtensions
     {
-        //public static void AddCustomServices(this IServiceCollection services)
-        //{
-        //    // unit if work
-        //    services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-        //    // services
 
-        //    services.AddScoped<IUserService, UserService>();
-        //    services.AddScoped<IMedicineService, MedicineService>();
-        //    services.AddScoped<ICategoryService, CategoryService>();
-        //    services.AddScoped<IOrderService, OrderService>();
-        //    services.AddScoped<IAuthService, AuthService>();
-        //}
+        public static void ConfigureJwt(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("Jwt");
 
-        //public static void ConfigureJwt(this IServiceCollection services, IConfiguration configuration)
-        //{
-        //    var jwtSettings = configuration.GetSection("Jwt");
+            string key = jwtSettings.GetSection("Key").Value;
 
-        //    string key = jwtSettings.GetSection("Key").Value;
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
 
-        //    services.AddAuthentication(options =>
-        //    {
-        //        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.GetSection("Issuer").Value,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
 
-        //        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        //    })
-        //    .AddJwtBearer(options =>
-        //    {
-        //        options.TokenValidationParameters = new TokenValidationParameters
-        //        {
-        //            ValidateIssuer = true,
-        //            ValidateAudience = false,
-        //            ValidateLifetime = true,
-        //            ValidateIssuerSigningKey = true,
-        //            ValidIssuer = jwtSettings.GetSection("Issuer").Value,
-        //            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+                };
+            });
+        }
 
-        //        };
-        //    });
-        //}
+        public static void AddSwaggerService(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(p =>
+            {
+                p.ResolveConflictingActions(ad => ad.FirstOrDefault());
+                p.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                });
 
-        //public static void AddSwaggerService(this IServiceCollection services)
-        //{
-        //    services.AddSwaggerGen(p =>
-        //    {
-        //        p.ResolveConflictingActions(ad => ad.FirstOrDefault());
-        //        p.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-        //        {
-        //            Name = "Authorization",
-        //            Type = SecuritySchemeType.ApiKey,
-        //            BearerFormat = "JWT",
-        //            In = ParameterLocation.Header,
-        //        });
-
-        //        p.AddSecurityRequirement(new OpenApiSecurityRequirement
-        //        {
-        //            {
-        //                new OpenApiSecurityScheme()
-        //                {
-        //                    Reference = new OpenApiReference()
-        //                    {
-        //                        Type = ReferenceType.SecurityScheme,
-        //                        Id = "Bearer"
-        //                    }
-        //                },
-        //                new string[] { }
-        //            }
-        //        });
-        //    });
-        //}
+                p.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme()
+                        {
+                            Reference = new OpenApiReference()
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+            });
+        }
     }
 }
