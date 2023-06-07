3fc7b793934ee2c643eb26a7a4307f807ddc1cc1
HamroqulovaMarjona <marjonabobomurodovna04@gmail.com>
2023-06-07 13:36:35 +0500

    add new changes

--- Pharmacy.Service/DTOs/UserForViewDTO.cs
+++ Pharmacy.Service/DTOs/UserForViewDTO.cs
@@ -2,6 +2,7 @@
 {
     public class UserForViewDTO
     {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Username { get; set; }
